21b410791a874849cc8a5b3a45fac2c64d2af986
Rachel Klein <rph.klein@gmail.com>
2025-11-30 07:25:21 -0800

    Fix formatting issues in Seq_QC_script.sh

--- Seq_QC_script.sh
+++ Seq_QC_script.sh
@@ -5,13 +5,13 @@
 # Takes two arguments: first the data directory where the fastq files are stored, second
 # the directory to output fastqc results into. Assumes files are in .fq.gz format, and 
 # F and R sequences are located together in a subfolder within the data directory.
+
 set -euo pipefail
 
 data_directory=$1
 fastqc_out=$2
 
 mkdir -p "$fastqc_out"
-
 
 find "$data_directory" -name '*.fq.gz' -print0 |
 	while IFS= read -r -d '' line; do
